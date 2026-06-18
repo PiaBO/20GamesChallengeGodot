4f0c687872bfc4d4e70d3e0238f9de0372459a1a
Pia <mpiabarcelo@gmail.com>
2026-06-18 13:38:09 +0200

    limites

--- 2/breakout/escenas/pelota.gd
+++ 2/breakout/escenas/pelota.gd
@@ -5,6 +5,8 @@ var acelearcion := 1.0
 func _ready() -> void:
 	position.x = GameManager.ANCHO_PANTALLA / 2
 	position.y = 20
+	
+	linear_velocity = Vector2(0,400)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
