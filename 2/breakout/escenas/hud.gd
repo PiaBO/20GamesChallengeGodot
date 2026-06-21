56c638b4b2cd8e8b0884e0cb220034a7b74687fd
pia <mpiabarcelo@gmail.com>
2026-06-21 18:03:13 +0200

    sistema de guardado

--- 2/breakout/escenas/hud.gd
+++ 2/breakout/escenas/hud.gd
@@ -2,6 +2,9 @@ extends CanvasGroup
 
 @onready var labelPuntos := $LabelPuntos
 @onready var labelVidas := $LabelVida
+@onready var puesto1 := $Puesto1
+@onready var puesto2 := $Puesto2
+@onready var puesto3 := $Puesto3
 
 func _ready() -> void:
 # Conectamos las señales del GameManager
@@ -19,4 +22,23 @@ func _on_vidas_cambiaron(nuevo_total: int) -> void:
 		1: labelVidas.text = "<3" 
 		
 
+func actualizar_ranking() -> void:
+	var ranking = GameManager.ranking
 	
+	# Puesto 1
+	if ranking.size() > 0:
+		puesto1.text = str(ranking[0].puntos) + " pts (Nivel " + str(ranking[0].nivel) + ")"
+	else:
+		puesto1.text = "---"
+	
+	# Puesto 2
+	if ranking.size() > 1:
+		puesto2.text = str(ranking[1].puntos) + " pts (Nivel " + str(ranking[1].nivel) + ")"
+	else:
+		puesto2.text = "---"
+	
+	# Puesto 3
+	if ranking.size() > 2:
+		puesto3.text = str(ranking[2].puntos) + " pts (Nivel " + str(ranking[2].nivel) + ")"
+	else:
+		puesto3.text = "---"
--- 2/breakout/game_manager.gd
+++ 2/breakout/game_manager.gd
@@ -13,10 +13,15 @@ var puntos := 0
 var vidas
 var esta_pausado := false
 
+const RUTA_RANKING = "user://ranking.json"
+var ranking: Array = []  # Array de diccionarios: [{puntos: 100, nivel: 1}, ...]
+
 func _ready() -> void:
 	ANCHO_PANTALLA = get_viewport_rect().size.x
 	ALTO_PANTALLA = get_viewport_rect().size.y
 	vidas = 3
+	
+	cargar_ranking()
 	
 func pausar_juego() -> void:
 	esta_pausado = true
@@ -45,9 +50,55 @@ func perder_vida() -> void:
 	if vidas <= 0:
 		vidas = 3
 		game_over.emit()
-
+		intentar_guardar_puntuacion(puntos, 1)
+		
 func reiniciar_partida() -> void:
 	puntos = 0
 	vidas = 3
 	puntos_cambiaron.emit(puntos)
 	vidas_cambiaron.emit(vidas)
+	get_tree().reload_current_scene()
+
+#guardar en fichero
+func cargar_ranking() -> void:
+	if not FileAccess.file_exists(RUTA_RANKING):
+		ranking = []
+		return
+	
+	var archivo = FileAccess.open(RUTA_RANKING, FileAccess.READ)
+	var texto = archivo.get_as_text()
+	archivo.close()
+	
+	var json = JSON.new()
+	var error = json.parse(texto)
+	if error == OK:
+		ranking = json.data
+	else:
+		ranking = []
+		push_warning("Error al cargar ranking: " + json.get_error_message())
+		
+func guardar_ranking() -> void:
+	var archivo = FileAccess.open(RUTA_RANKING, FileAccess.WRITE)
+	archivo.store_string(JSON.stringify(ranking, "\t"))  # "\t" para formato legible
+	archivo.close()
+
+func intentar_guardar_puntuacion(puntos: int, nivel: int = 1) -> bool:
+	var nueva = {"puntos": puntos, "nivel": nivel}
+	
+	# Insertar en la posición correcta (ordenado de mayor a menor)
+	ranking.append(nueva)
+	ranking.sort_custom(func(a, b): return a.puntos > b.puntos)
+	
+	# Mantener solo las 3 mejores
+	if ranking.size() > 3:
+		ranking.resize(3)
+	
+	guardar_ranking()
+	return nueva in ranking
+
+func es_top_3(puntos: int) -> bool:
+	if ranking.size() < 3:
+		return true
+	return puntos > ranking[2].puntos
+	
+	
